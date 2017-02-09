7b73a84ed862646b9399e529ccd4050a675321f5
Peter Girouard <plgiroua@ncsu.edu>
2017-02-08 21:06:08 -0500

    Added functionality of a timer and pause button

--- Taboo/Taboo/ViewController.swift
+++ Taboo/Taboo/ViewController.swift
@@ -10,23 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer:Timer!
+    var time = 60
+    var paused = false
+    @IBOutlet weak var clock: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        clock.text = "\(time)"
+        makeTimer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func makeTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.decClock), userInfo: nil, repeats: true)
+    }
+    
+    func decClock() {
+        if(time == 0) {
+            timer.invalidate()
+        }
+        else {
+            time -= 1
+            clock.text = "\(time)"
+        }
+    }
+    
+    func fillTable() {
+        
+    }
 
     @IBAction func onPauseClick(_ sender: AnyObject) {
+        if(paused) {
+            makeTimer()
+            paused = false
+        }
+        else {
+            timer.invalidate()
+            paused = true
+        }
     }
 
     @IBAction func onBadClick(_ sender: AnyObject) {
+        print("WRONG")
     }
     
     @IBAction func onGoodClick(_ sender: AnyObject) {
+        print("CORRECT")
     }
 }
 
